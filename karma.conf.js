8cdf03a5d23774da4e2110ba858ea5bbf86725aa
Jaunius Belickas <jauniusb@wix.com>
2017-08-30 08:22:18 +0300

    test runs ;

--- karma.conf.js
+++ karma.conf.js
@@ -15,8 +15,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      '"src/*.js"',
-      '"test/*spec.js"'
+      'src/*.js',
+      'test/*spec.js'
     ],
 
 
