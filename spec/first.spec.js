05f7ccd14c20ccbeba89295d90587b656f0384de
Jaunius Belickas <jauniusb@wix.com>
2017-08-30 08:47:53 +0300

    wip, tests do not see doc

--- spec/first.spec.js
+++ spec/first.spec.js
@@ -1,11 +1,10 @@
-//require('../src/mylib.js');
-
 describe("my suite", function() {
 	it("should do stuff", function() {
 		expect(1).toBe(1);
 	});
 	it("should do stuff2", function() {
-		init();		
+		//init();		
+		console.log("mydoc", document.getElementById('myDiv'));
 		expect(1).toBe(1);
 	});
 
