90a5e180974e1baf0b96a1501444e7ea9a9a86b5
Jaunius Belickas <jauniusb@wix.com>
2017-08-30 08:38:35 +0300

    added spec, tests run, do not see lib

---
+++ spec/first.spec.js
@@ -0,0 +1,12 @@
+//require('../src/mylib.js');
+
+describe("my suite", function() {
+	it("should do stuff", function() {
+		expect(1).toBe(1);
+	});
+	it("should do stuff2", function() {
+		init();		
+		expect(1).toBe(1);
+	});
+
+});
