0dfce9fe5db902e09402945b0b100ffe6e858078
Jaunius Belickas <jauniusb@wix.com>
2017-08-30 08:27:42 +0300

    Initial lib, not included in tests. Green

--- karma.conf.js
+++ karma.conf.js
@@ -16,12 +16,13 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'src/*.js',
-      'test/*spec.js'
+      'spec/*spec.js'
     ],
 
 
     // list of files to exclude
     exclude: [
+	'spec/support/**/*.jasmine.json'
     ],
 
 
@@ -61,7 +62,7 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: true,
 
     // Concurrency level
     // how many browser should be started simultaneous
