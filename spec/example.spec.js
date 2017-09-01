6ef8144466fb4dd8c74a083a653f267c574379e1
Jaunius <jaunius@gmail.com>
2017-09-01 23:26:01 +0300

    add sinon for mocks

--- spec/example.spec.js
+++ spec/example.spec.js
@@ -15,5 +15,33 @@ describe("my suite", function () {
         // 1.should.not.equal 2;
         // should.exist 123;
     });
+
+    it('sinon: calls the original function', function () {
+        var myAPI = { method: function () {} };
+
+        var mock = sinon.mock(myAPI);
+        mock.expects("method").once().withArgs(10);
+        myAPI.method(10);
+        mock.verify();
+    });
 });
 
+describe("sinon example test", function () {
+    var time2013_10_01;
+
+    time2013_10_01 = (new Date(2013, 10-1, 1)).getTime();
+
+    beforeEach(function() {
+        // sinon was defined in global scope
+        this.fakeTimer = new sinon.useFakeTimers(time2013_10_01);
+    });
+
+    it("some test", function() {
+        //test
+    });
+
+    afterEach(function() {
+        this.fakeTimer.restore();
+    });beforeEach
+
+});
