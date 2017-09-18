49478477621a3a5ffda0d6a3f9bcc289c469f017
Liudo personal <LiudoPersonal@MacBook-Pro-3.local>
2017-09-18 23:52:02 +0300

    Wicked wicked, refactor is massive

--- src/vaski-hop.js
+++ src/vaski-hop.js
@@ -4,6 +4,17 @@ var scissors, scissors2, paper, paper2, well,well2, continueButton, resetButton,
 
 var setComputerSelection = function () {
     computerSelection = Math.floor(Math.random() * 3) + 1;
+	switch (computerSelection){
+		case 1: 
+			computerSelection = 'scissorsSelected';
+			break;
+		case 2:
+			computerSelection = 'paperSelected';
+			break;
+		case 3:
+			computerSelection = 'wellSelected';
+			break;
+	};
 };
 
 var init = function () {
@@ -41,16 +52,13 @@ function initializeVariables() {
 	playerName = document.getElementById('playerName');
 } 
 
-//function assignComputerSelection() {
-//    computerSelection = setComputerSelection();
-//} 
 
 function addEventListeners() {
     scissors.addEventListener("click", function () {
         scissors.classList.toggle('active');
         paper.classList.remove('active');
         well.classList.remove('active');
-        playerSelection = 1;
+        playerSelection = 'scissorsSelected';
         showPlayerSelection();
     });
 
@@ -58,7 +66,7 @@ function addEventListeners() {
         scissors.classList.remove('active');
         paper.classList.toggle('active');
         well.classList.remove('active');
-        playerSelection = 2;
+        playerSelection = 'paperSelected';
         showPlayerSelection();
     });
 
@@ -66,7 +74,7 @@ function addEventListeners() {
         scissors.classList.remove('active');
         paper.classList.remove('active');
         well.classList.toggle('active');
-        playerSelection = 3;
+        playerSelection = 'wellSelected';
         showPlayerSelection();
     });
 
@@ -86,27 +94,47 @@ var showPlayerSelection = function () {
     playerName.innerHTML = "Your selection is " + playerSelection;
 };
 
-	
-function winner() {
-	
-	
-    if (playerSelection === computerSelection){
-        window.alert("IT'S A BLOODY DRAW");
-    } else if (
-        playerSelection === 1 && computerSelection === 2 ||
-        playerSelection === 2 && computerSelection === 3 ||
-        playerSelection === 3 && computerSelection === 1) {
-        window.alert("PLAYAR WINS");
-    } else if (
-        playerSelection === 1 && computerSelection === 3 ||
-        playerSelection === 2 && computerSelection === 1 ||
-        playerSelection === 3 && computerSelection === 2) {
-        window.alert("COMPUTAR WINS");
-    }
+var gameOutcome, playerWins, computerWins, draw;
 
-}
+function calcWinner (){
+	switch (true) {
+		case playerSelection === computerSelection:
+			gameOutcome = 'draw';
+			break;
+			
+		case playerSelection === 'scissorsSelected' && computerSelection === 'paperSelected':
+		case playerSelection === 'paperSelected' && computerSelection === 'wellSelected':
+		case playerSelection === 'wellSelected' && computerSelection === 'scissorsSelected':
+			gameOutcome = 'playerWins';
+			break;
+		
+		case playerSelection === 'scissorsSelected' && computerSelection === 'wellSelected':
+		case playerSelection === 'paperSelected' && computerSelection === 'scissorsSelected':
+        case playerSelection === 'wellSelected' && computerSelection === 'paperSelected':
+			gameOutcome = 'computerWins';
+			break;
+		}
+};
 
-// If player choice is done, reveals computer selection, removes continue button, declares winner
+function announceGameResult(){
+	switch (gameOutcome) {
+	  case 'playerWins':
+		window.alert("PLAYAR WINS");
+		break; 		
+
+	  case 'computerWins':
+		window.alert("COMPUTAR WINS");
+		break;
+
+	  case 'draw':
+		window.alert("IT'S A BLOODY DRAW");		
+		break;
+
+	  default:
+		window.alert("Oops something went wrong");
+	};
+};
+		
 
 var displayComputerSelection = function () {
 
@@ -119,13 +147,14 @@ var displayComputerSelection = function () {
         setComputerSelection();
         continueButton.classList.add('hidden');
 
-        if (computerSelection == 1) {
+        if (computerSelection == 'scissorsSelected') {
            scissors2.classList.remove('hidden')
         } else if (computerSelection == 2) {
             paper2.classList.remove('hidden')
-        } else if (computerSelection == 3) {
+        } else if (computerSelection == 'wellSelected') {
             well2.classList.remove('hidden')
         }
-        winner();
+        calcWinner();
+		announceGameResult();
     }
 };
